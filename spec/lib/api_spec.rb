ac206574d5c5e62db90b92293f2b9eea82e8a407
Meinhard Gredig <mg@flexcoding.de>
2017-10-06 23:15:25 +0200

    Refactor specs

--- spec/lib/api_spec.rb
+++ spec/lib/api_spec.rb
@@ -3,6 +3,9 @@
 require 'spec_helper'
 
 describe Cleverreach::API, :vcr do
+  let(:credentials) { Cleverreach::Credentials.new('123456', 'user', 'pass') }
+  let(:api) { Cleverreach::API.new(credentials) }
+
   describe '#initialize' do
     context 'fails' do
       it 'without credentials' do
@@ -16,9 +19,7 @@ describe Cleverreach::API, :vcr do
 
     context 'succeeds' do
       it 'with credentials' do
-        credentials = Cleverreach::Credentials.new('123456', 'user', 'pass')
-        actual = Cleverreach::API.new(credentials)
-        expect(actual).to be_an_instance_of Cleverreach::API
+        expect(api).to be_an_instance_of Cleverreach::API
       end
     end
   end
@@ -27,16 +28,12 @@ describe Cleverreach::API, :vcr do
     VCR.use_cassette('login') do
       context 'fails' do
         it 'with 400 response' do
-          credentials = Cleverreach::Credentials.new('123456', 'user', 'pass')
-          api = Cleverreach::API.new(credentials)
           expect { api.login }.to raise_error RestClient::ExceptionWithResponse
         end
       end
 
       context 'succeeds' do
         it 'with auth token response' do
-          credentials = Cleverreach::Credentials.new('123456', 'user', 'pass')
-          api = Cleverreach::API.new(credentials)
           api.login
           expect(api.token).to eq('"ey123"')
         end
@@ -48,22 +45,16 @@ describe Cleverreach::API, :vcr do
     VCR.use_cassette('subscribe') do
       context 'fails' do
         it 'with 405 response for invalid group' do
-          credentials = Cleverreach::Credentials.new('123456', 'user', 'pass')
-          api = Cleverreach::API.new(credentials)
           expect { api.subscribe('email@domain.de', '654322') }.to raise_error RestClient::ExceptionWithResponse
         end
       end
 
       context 'succeeds' do
         it 'with 200 insert success' do
-          credentials = Cleverreach::Credentials.new('123456', 'user', 'pass')
-          api = Cleverreach::API.new(credentials)
           expect(api.subscribe('bruce@gotham.com', '654321')).to be_truthy
         end
 
         it 'with 200 insert success including source' do
-          credentials = Cleverreach::Credentials.new('123456', 'user', 'pass')
-          api = Cleverreach::API.new(credentials)
           expect(api.subscribe('bruce@gotham.de', '654321', 'rspec')).to be_truthy
         end
       end
@@ -74,16 +65,12 @@ describe Cleverreach::API, :vcr do
     VCR.use_cassette('unsubscribe') do
       context 'fails' do
         it 'with 404 response' do
-          credentials = Cleverreach::Credentials.new('123456', 'user', 'pass')
-          api = Cleverreach::API.new(credentials)
           expect { api.unsubscribe('email@domain.de', '654321') }.to raise_error RestClient::ExceptionWithResponse
         end
       end
 
       context 'succeeds' do
         it 'with 200 response' do
-          credentials = Cleverreach::Credentials.new('123456', 'user', 'pass')
-          api = Cleverreach::API.new(credentials)
           expect(api.unsubscribe('bruce@gotham.com', '654321')).to be_truthy
         end
       end
